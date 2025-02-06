af1681f4392219078d4a76c3ca523a52a61f47f6
Ps066 <prashantshukla0666@gmail.com>
2025-02-06 11:01:58 +0530

    Db  connected

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-console.log(" we are ready to run ");
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDb from './db/db.js';
+
+dotenv.config();
+
+connectDb();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // an approach to connect the db using the Invoked Function Expression (IIFE)
+// const app = express();
+// dotenv.config();
+// ;(async ()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`);
+//         app.on("error", (error)=>{
+//             console.log("Err:", error);
+//             throw error;            
+//         })
+
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`App is listening on the port ${process.env.PORT}`);            
+//         })
+//     } catch (error) {
+//         console.log("Err:", error);
+//         throw error;
+//     }
+// })()
